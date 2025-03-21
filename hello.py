ef62fe79672e05aafd752c816c49c01952e47ae8
Avinash Magar <avinashmagar15@gmail.com>
2025-03-21 13:53:50 +0530

    new change

--- hello.py
+++ hello.py
@@ -1,7 +1,5 @@
 import streamlit as st
-
 st.write("Hello Avinash , Welcome In Streamlit") # for output msg
 name_movie=st.text_input('Favourite Movie')
 st.write(f"Your favourite movie is {name_movie}")
-
 is_click=st.button("Click Me")
