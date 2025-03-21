0eefaa387f2c5777d47ed04dccc1a578219d5d0b
Avinash Magar <avinashmagar15@gmail.com>
2025-03-21 13:52:39 +0530

    new change

--- hello.py
+++ hello.py
@@ -1,9 +1,7 @@
 import streamlit as st
-import seaborn as sns
+
 st.write("Hello Avinash , Welcome In Streamlit") # for output msg
 name_movie=st.text_input('Favourite Movie')
 st.write(f"Your favourite movie is {name_movie}")
-df=sns.load_dataset('iris')
-st.write(df)
-st.bar_chart(df)
+
 is_click=st.button("Click Me")
